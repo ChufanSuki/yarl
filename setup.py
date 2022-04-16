c066dd2abfe0d8d47d1c453d93cc41beabc02587
Chufan Chen <allenplato28@gmail.com>
2022-04-16 03:10:36 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='yarl',
+    version='0.1.0',
+    description='Yet Another Reinforcement Learning',
+    author='Chufan Chen',
+    author_email='ccf@chufanchen.com',
+    packages=['yarl'],
+)
